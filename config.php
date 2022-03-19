5753de4352e02bb1a8fb33edd2a6c52baad7c16d
is41-shakirovaash <shamilevna74@gmail.com>
2022-03-19 11:42:27 +0500

    Version 1.0.7

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOSTNAME = 'mysqlis.radiotech.su';
 const DB_USERNAME = 'is341_22_login';
-const DB_PASSWORD = 'T6evASytyT';
+const DB_PASSWORD = '123456';
 const DB_DATABASE = 'is341_22_Shakirova';
 const DB_PORT = '3306'; # Default 3306
